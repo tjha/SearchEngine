d42ebe914ebec9d1bef0c5ecc1beb513e9502367
tjha <tjha@umich.edu>
2019-09-12 19:38:16 -0400

    Introduced bug in example.cpp to test build fail

--- example.cpp
+++ example.cpp
@@ -1,15 +1,12 @@
 // example.cpp
 // 
 // Example "Hello World" program to test Travis CI workflow
-//
-// History:
-//    - 12 September 2019: Tejas Jha implemented "Hello World"
 #include<iostream>
 
 
 int main()
    {
-   std::cout << "Hello World!" << std::endl;
+   std::cout << "Hello World!" << std::endl
    return 0;
    }
 
