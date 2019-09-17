65ba23e95cc1cda6e0fa9e0f0bd93184868efa4b
jhirshey <jhirshey@umich.edu>
2019-09-17 16:55:47 -0400

    Implememented insert, size, and clear
    ready for some review, and general discussion about other possible
    implementations

--- utils/vector.hpp
+++ utils/vector.hpp
@@ -2,6 +2,7 @@
 // 
 // Vector class
 //
+// 2019/09/17 - Jonas Hirshland implemented insert, clear, grow, size
 // 2019/09/14 - Tejas Jha created basic skeleton with private members
 
 namespace dex
@@ -10,12 +11,40 @@ namespace dex
    class vector
       {
       public:
+      void insert(int index, T obj)
+         {
+         if ( size + 1 > capacity / 2 ) 
+            {
+            grow() // you might not want to do a simple grow when inserting
+            }
+         
+         T old;
+         for ( int i = index;  i <= size;  ++i )
+            {
+               old = arr[ index ];
+               arr[ index ] = old;
+               obj = old;
+            }
+         }
+      void clear()
+         {
+         T *ptr = arr;
+         for ( int i = index;  i < size;  ++i )
+            {
+               *ptr = nullptr; 
+            }
+         size = 0;
+         }
       void grow()
          {
          capacity *= 2;
          T *arr_old = &arr;
          arr = new T[capacity];
          delete(arr_old);
+         }
+      int size() 
+         {
+         return size;
          }
       private:
       T *arr;
