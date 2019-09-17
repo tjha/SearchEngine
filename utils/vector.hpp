4af164dbb45b968466f0e816177710cfed758679
jhirshey <jhirshey@umich.edu>
2019-09-17 16:07:33 -0400

    Added grow function to dex::vector
    - also added #include to src/example.cpp to make sure utils/vector.hpp compiles
    
    branched from vector/dex/init-commit

--- utils/vector.hpp
+++ utils/vector.hpp
@@ -6,9 +6,17 @@
 
 namespace dex
    {
-   template <class T>
+   template < class T >
    class vector
       {
+      public:
+      void grow()
+         {
+         capacity *= 2;
+         T *arr_old = &arr;
+         arr = new T[capacity];
+         delete(arr_old);
+         }
       private:
       T *arr;
       int capacity;
