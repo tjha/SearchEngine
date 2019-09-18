820ffe538b50309d9ae3b10bf972380a2d3a4dd3
Tejas Jha <tjha@umich.edu>
2019-09-18 17:45:05 -0400

    Basic Constructors and Destructor for Vector (#6)
    * Implemented vector ctors + dtors + tests
    
    Implemented constructors and destructors for vector. Specifically
    implemented default ctor, specified size ctor, and specified size with
    default values ctor.
    
    Also implemented basic testing in catch2 test file to ensure vector
    objects can be created and destroyed.
    
    * Modified Makefile to build and test vector class
    
    * Modified Makefile command namees to describe usage
    
    Added build_sources, build_tests, run_unit_tests
    Modified .travis.yml to run appropriate tests
    
    TODO: add run_integration_tests once they are created

--- utils/vector.hpp
+++ utils/vector.hpp
@@ -2,16 +2,59 @@
 // 
 // Vector class
 //
-// 2019/09/14 - Tejas Jha created basic skeleton with private members
+// 2019/09/17 - Tejas Jha - created constructors and destructor
+//                        - Added comment descriptions for private variables
+//                        - modified ints to unsigned int
+// 2019/09/14 - Tejas Jha - created basic skeleton with private members
 
 namespace dex
    {
-   template <class T>
+   template < class T >
    class vector
       {
+      public:
+      vector( );
+      vector( unsigned int num );
+      vector ( unsigned int num, const T& val );
+      ~vector( );
+
       private:
-      T *arr;
-      int capacity;
-      int size;
+      T *arr;                  // dynamic array 
+      unsigned int capacity;   // amount of available space
+      unsigned int size;       // number of elemets added to array 
       };
+
+   template < class T >
+   // default constructor
+   vector< T >::vector( )
+      {
+      arr = new T[ 1 ];
+      capacity = 1;
+      size = 0;
+      }
+
+   template < class T >
+   // constructor for vector of num elements
+   vector< T >::vector( unsigned int num )
+      {
+      arr = new T[ num + 1 ]( );
+      capacity = num + 1;
+      size = num;
+      }
+
+   template < class T >
+   // constructor for vector of num elements each initialized to val
+   vector< T >::vector( unsigned int num, const T& val )
+      {
+      arr = new T[ num + 1 ] { val };
+      capacity = num + 1;
+      size = num;
+      }
+
+   template < class T >
+   // destructor
+   vector< T >::~vector( )
+      {
+      delete[ ] arr;
+      }
    }
