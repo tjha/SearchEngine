da3b356a6e3e3b8dc3e2edff7308be86927d404d
Chris Combs <combsc@umich.edu>
2019-10-21 15:52:18 -0400

    Add tests for poorly formatted paths, moved to catch2 format
    combsc

---
+++ tst/robotTxtTests.cpp
@@ -0,0 +1,45 @@
+// robotTxtTests.cpp
+// Testing for our robots class
+//
+// 2019-10-21: File creation: combsc
+
+#include "catch.hpp"
+#include <unistd.h>
+#include "../src/spinarak/robots.hpp"
+
+using dex::RobotTxt;
+using std::string;
+
+TEST_CASE( "timing for visiting sites", "[robotsTxt]" )
+	{
+	string url = "https://domain.com";
+   RobotTxt hello = RobotTxt( url, 1 );
+	REQUIRE( !hello.canVisitPath( "/" ) );
+   sleep(2);
+	REQUIRE( hello.canVisitPath( "/" ) );
+
+	RobotTxt henlo = RobotTxt( url, 1 );
+	henlo.addPathsDisallowed( "/" );
+	REQUIRE( !henlo.canVisitPath( "/" ) );
+   sleep(2);
+	REQUIRE( !henlo.canVisitPath( "/" ) );
+	}
+
+TEST_CASE( "whitelist and blacklist rules", "[robotsTxt]" )
+	{
+	string url = "https://domain.com";
+   RobotTxt hello = RobotTxt( url, 0 );
+	sleep(1);
+	hello.addPathsDisallowed( "/" );
+	hello.addPathsAllowed( "/secret/path/" );
+	REQUIRE( !hello.canVisitPath( "/some/other/path/" ) );
+	REQUIRE( !hello.canVisitPath( "/" ) );
+	REQUIRE( hello.canVisitPath( "/secret/path/" ) );
+	REQUIRE( hello.canVisitPath( "/secret/path/somefile/" ) );
+
+	// testing passing "bad" paths
+
+	hello.addPathsAllowed ( "secret/other/path" );
+	REQUIRE( hello.canVisitPath( "secret/other/path" ) );
+	REQUIRE( hello.canVisitPath( "secret/other/path/file" ) );
+	}
