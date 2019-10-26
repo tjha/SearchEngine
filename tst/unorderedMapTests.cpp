920e3beec3419d120063749d2452eedb8b55b9f3
Stephen Jasina <jasina@umich.edu>
2019-10-26 17:03:22 -0400

    Write and pass element access tests
    jasina

--- tst/unorderedMapTests.cpp
+++ tst/unorderedMapTests.cpp
@@ -2,7 +2,7 @@
 //
 // Perform tests on unordered map implementation
 //
-// 2019-10-26: file created, constructors, assignment: jasina
+// 2019-10-26: file created, constructors, assignment, element access: jasina
 
 #include "catch.hpp"
 #include "../utils/basicString.hpp"
@@ -86,8 +86,76 @@ TEST_CASE( "constructors and assignment ", "[unorderedMap]" )
 		REQUIRE( map2[ "hola" ] == "mundo" );
 		REQUIRE( map2[ "bonjour" ] == "monde" );
 
+		map2 = map2;
+		REQUIRE( map2.size( ) == 3 );
+		REQUIRE( map2[ "hello" ] == "world" );
+		REQUIRE( map2[ "hola" ] == "mundo" );
+		REQUIRE( map2[ "bonjour" ] == "monde" );
+
 		map2 = map1;
 		REQUIRE( map2.empty( ) );
 		REQUIRE( map2.count( "hello" ) == 0 );
+
+		map2 = map2;
+		REQUIRE( map2.empty( ) );
+		REQUIRE( map2.count( "hello" ) == 0 );
+		}
+	}
+
+TEST_CASE( "element access", "[unorderedMap]" )
+	{
+	SECTION( "operator [ ]")
+		{
+		unorderedMap < string, unsigned > map;
+		REQUIRE( map.count( "beta" ) == 0 );
+		map[ "beta" ] = 1;
+		REQUIRE( map.count( "beta" ) == 1 );
+		REQUIRE( map.size( ) == 1 );
+
+		REQUIRE( map.count( "gamma" ) == 0 );
+		map[ "gamma" ] = 2;
+		REQUIRE( map.count( "gamma" ) == 1 );
+		REQUIRE( map.size( ) == 2 );
+
+		REQUIRE( map.count( "delta" ) == 0 );
+		map[ "delta" ] = 3;
+		REQUIRE( map.count( "delta" ) == 1 );
+		REQUIRE( map.size( ) == 3 );
+
+		REQUIRE( map.count( "epsilon" ) == 0 );
+		map[ "epsilon" ] = 4;
+		REQUIRE( map.count( "epsilon" ) == 1 );
+		REQUIRE( map.size( ) == 4 );
+
+		REQUIRE( map[ "alpha" ] == 0 );
+		REQUIRE( map.size( ) == 5 );
+		REQUIRE( map[ "beta" ] == 1 );
+		REQUIRE( map[ "gamma" ] == 2 );
+		REQUIRE( map[ "delta" ] == 3 );
+		REQUIRE( map[ "epsilon" ] == 4 );
+		REQUIRE( map.size( ) == 5 );
+		}
+	SECTION( "at")
+		{
+		unorderedMap < string, unsigned > map;
+		map[ "beta" ] = 1;
+		REQUIRE( map.size( ) == 1 );
+		map[ "gamma" ] = 2;
+		REQUIRE( map.size( ) == 2 );
+		map[ "delta" ] = 3;
+		REQUIRE( map.size( ) == 3 );
+		map[ "epsilon" ] = 4;
+		REQUIRE( map.size( ) == 4 );
+
+		REQUIRE_THROWS_AS( map.at( "alpha" ), dex::outOfRangeException );
+		REQUIRE( map.size( ) == 4 );
+		REQUIRE( map.at( "beta" ) == 1 );
+		REQUIRE( map.at( "gamma" ) == 2 );
+		REQUIRE( map.at( "delta" ) == 3 );
+		REQUIRE( map.at( "epsilon" ) == 4 );
+		REQUIRE( map.size( ) == 4 );
+
+		map.at( "beta" ) = 37;
+		REQUIRE( map.at( "beta" ) == 37 );
 		}
 	}
