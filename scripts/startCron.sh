d1e8a50aed3081440e644f82f5c445491c423bb6
EC2 Default User <ec2-user@ip-172-31-49-161.ec2.internal>
2019-12-06 04:37:39 +0000

    tuned cronjob. Simply run startCron.sh to have driver permenantly running on this instance
    comscb

--- scripts/startCron.sh
+++ scripts/startCron.sh
@@ -2,5 +2,6 @@
 
 kill -15 $(ps -e | grep "driver.exe" | grep -v grep | awk '{ print $1 }')
 cd $PATH_TO_SEARCH_ENGINE
-./build/driver.exe &
-echo done
+./build/driver.exe > cronresult.txt &
+crontab scripts/runOnCron.txt
+
