ed3f3ed53a12f27fe11ffd82dc6fdd04213a122d
Chris Combs <combsc@umich.edu>
2019-12-06 17:54:05 -0500

    error prints to cerr instead of cout
    combsc

--- src/mvp/driver.cpp
+++ src/mvp/driver.cpp
@@ -395,7 +395,7 @@ int main( )
 			instanceId = instanceInfo.second;
 			if ( numInstances == 0 && instanceId == 0 )
 				{
-				print( "Need to have file in data/instanceInfo.txt with the instance information.\nFirst line must contain numInstances=someNum, second line must contain instanceId=someNum." );
+				std::cerr << "Need to have file in data/instanceInfo.txt with the instance information.\nFirst line must contain numInstances=someNum, second line must contain instanceId=someNum.\n";
 				return 0;
 				}
 			signal(SIGPIPE, SIG_IGN);
