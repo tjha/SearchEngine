e177da3c5aff2364dfc1b5ecfc59d043b275e40b
jhirshey <jhirshey@umich.edu>
2019-12-06 17:24:08 -0500

    Separated performance and saving frontier
    jhirsh

--- scripts/startCron.sh
+++ scripts/startCron.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+PATH_TO_SEARCH_ENGINE=$(pwd)
 kill -15 $(ps -e | grep "driver.exe" | grep -v grep | awk '{ print $1 }')
 cd $PATH_TO_SEARCH_ENGINE
 ./build/driver.exe > cronresult.txt &
