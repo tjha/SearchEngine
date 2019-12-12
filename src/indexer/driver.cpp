c764ef8c749d9e32c303772ba47f110c997647a7
Matthew Lougheed <cocoshrimp16@gmail.com>
2019-12-11 20:11:12 -0500

    Create index chunks and change how to move through directories
    Matthew

--- src/indexer/driver.cpp
+++ src/indexer/driver.cpp
@@ -7,9 +7,10 @@
 #include <unordered_set>
 #include "dirent.h"
 #include "index.hpp"
+#include "exception.hpp"
 #include "file.hpp"
 #include "vector.hpp"
-#include "string.hpp"
+#include "basicString.hpp"
 
 using dex::string;
 using dex::vector;
@@ -26,14 +27,22 @@ int main ( int argc, char ** argv )
 	string batch = argv[ 1 ];
 	string outputFolder = argv[ 2 ];
 
-	unordered_set < char * > toProcess;
-	Dir * dir;
-	dir = opendir( batch );
+	dex::unorderedSet < char * > toProcess;
+	DIR * dir;
+	dir = opendir( batch.cStr( ) );
 	dirent * entry = readdir( dir );
+
+	string directory( "." );
+	string parentDirectory( ".." );
 
 	while ( entry != NULL )
 		{
-		if ( strcmp( entry->d_name, "." ) != 0 && strcmp( entry->d_name, "." ) != 0 && entry->d_type == DT_DIR )
+		string direntryName( entry->d_name );
+		if ( dex::lexicographicalCompare( direntryName.cbegin( ), direntryName.cend( ),
+				directory.cbegin( ), directory.cend( ) )
+				&& dex::lexicographicalCompare( direntryName.cbegin( ), direntryName.cend( ),
+				parentDirectory.cbegin( ), parentDirectory.cend( ) ) 
+				&& entry->d_type == DT_DIR )
 			{
 			// look at the directory
 			toProcess.insert( entry->d_name );
@@ -46,7 +55,7 @@ int main ( int argc, char ** argv )
 	int indexChunkCount = 0;
 	// TODO: What scheme will we use to name the files for the index chunks?
 	int fileDescriptor = openFile( indexChunkCount++ );
-	indexChunk *initializingIndexChunk = indexChunk( fileDescriptor );
+	dex::indexChunk *initializingIndexChunk = indexChunk( fileDescriptor );
 
 	for ( auto &directory: toProcess )
 		{
@@ -65,12 +74,24 @@ int main ( int argc, char ** argv )
 				//...
 				dex::HTMLparser parser( url, html, true );
 
-				initializingIndexChunk->addDocument( url, "", parser.ReturnAnchorText( ), parser.ReturnTitle( ) )
+				if ( !initializingIndexChunk->addDocument( url, parser.ReturnAnchorText( ), parser.ReturnTitle( ),
+						parser.ReturnWords( ) ) )
+					{
+					close( fileDescriptor );
+					fileDescriptor = openFile( indexChunkCount++ );
+					initializingIndexChunk = indexChunk( fileDescriptor );
+					}
+				if ( !initializingIndexChunk->addDocument( url, parser.ReturnAnchorText( ), parser.ReturnTitle( ),
+						parser.ReturnWords( ) ) )
+					{
+					// TODO: Throw an exception. Should not fail to add a document to a new index chunk
+					throw dex::fileWriteException;
+					}
 				}
 			}
-		
 		close( dir );
 		}
+	close( fileDescriptor );
 	}
 
 int openFile( int indexChunkCount )
