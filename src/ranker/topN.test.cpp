35414093d5dda2461b138cd7672e82a800322909
Stephen Jasina <jasina@umich.edu>
2019-12-29 21:41:12 -0500

    Fix memory leak (#64)
    * Fix memory leak
    
    jasina
    Additionally avoid using linked lists
    
    * Make topN.hpp more readable
    
    jasina
    
    * Remove output.txt that was accidentally added
    
    jasina
    
    * Add another test case
    
    jasina

--- src/ranker/topN.test.cpp
+++ src/ranker/topN.test.cpp
@@ -3,30 +3,49 @@
 //
 // 2019-12-11: Init commit: lougheem
 
-#include <iostream>
+#include <cstddef>
 #include "catch.hpp"
 #include "ranker/topN.hpp"
 
 TEST_CASE( "topN" )
 	{
-	dex::vector< double > scores;
-	dex::vector< double > topScores;
-	int m = 1000;
-
-	for ( double index = 0;  index < m + 1;  index++ )
+	SECTION( "reversed" )
 		{
-		scores.pushBack( index );
-		//std::cout << "score: " << index << "\n";
+		dex::vector < double > scores;
+
+		for ( size_t index = 0;  index < 1500;  ++index )
+			scores.pushBack( static_cast < double >( index ) / 2 );
+
+		size_t N = 100;
+		dex::documentInfo **topN = dex::findTopN( scores, N );
+		for ( size_t index = 0;  index < N;  ++index )
+			REQUIRE( topN[ index ]->score == static_cast < double >( 1499 - index ) / 2 );
+
+		for ( size_t index = 0;  index < N;  ++index )
+			delete topN[ index ];
+		delete [ ] topN;
 		}
 
-	REQUIRE( scores.size( ) >= m );
-	int n = m/10;
-	documentInfo **topN, *p;
-	topN = findTopN( scores, n );
-	for ( int index = 0;  index < n - 1;  index++ )
+	SECTION( "gamma" )
 		{
-		REQUIRE( topN[ index ]->score > topN[ index + 1 ]->score );
-		}
+		dex::vector < double > scores = { 0, 5, 7, 7, 2, 1, 5, 6, 6, 4, 9 };
 
+		size_t N = 5;
+		dex::documentInfo **topN = dex::findTopN( scores, N );
+		REQUIRE( topN[ 0 ]->score == 9 );
+		REQUIRE( topN[ 0 ]->documentIndex == 10 );
+		REQUIRE( topN[ 1 ]->score == 7 );
+		REQUIRE( topN[ 1 ]->documentIndex == 2 );
+		REQUIRE( topN[ 2 ]->score == 7 );
+		REQUIRE( topN[ 2 ]->documentIndex == 3 );
+		REQUIRE( topN[ 3 ]->score == 6 );
+		REQUIRE( topN[ 3 ]->documentIndex == 7 );
+		REQUIRE( topN[ 4 ]->score == 6 );
+		REQUIRE( topN[ 4 ]->documentIndex == 8 );
+
+		for ( size_t index = 0;  index < N;  ++index )
+			delete topN[ index ];
+		delete [ ] topN;
+		}
 	}
 
