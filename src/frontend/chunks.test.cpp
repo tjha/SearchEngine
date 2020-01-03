b51f77410cfe0c31f3941f571745ceb5b1fe2215
Chris Combs <combsc@umich.edu>
2020-01-03 14:18:48 -0500

    build our own index chunks instead of relying on pre-built ones
    combs

--- src/frontend/chunks.test.cpp
+++ src/frontend/chunks.test.cpp
@@ -1,6 +1,6 @@
 // file for testing chunks
 // Not going to use catch2 format yet.
-#include "indexer/index.hpp"
+#include "indexer/indexer.hpp"
 #include "ranker/ranker.hpp"
 #include "utils/basicString.hpp"
 #include "utils/vector.hpp"
@@ -42,23 +42,46 @@ int main( int argc, char **argv )
 	{
 	if ( argc < 2 )
 		{
-		std::cerr << "Usage: build/frontend/chunks.test.exe pathToChunks" << std::endl;
+		std::cerr << "Usage: build/frontend/chunks.test.exe tst/pathToChunksToBuild" << std::endl;
 		return 1;
 		}
 
-	// Find index chunks in the folder given
-	if ( findIndexChunks( argv[ 1 ], "_in.dex" ) == -1 )
-		return 1;
+	dex::string filePath = dex::string ( argv[ 1 ] ) + "test_in.dex";
+	int fd = open( filePath.cStr( ), O_RDWR | O_CREAT | O_TRUNC, 0777 );
+
+	if ( fd == -1 )
+		exit( 1 );
+
+		{
+		dex::index::indexChunk initializingIndexChunk = dex::index::indexChunk( fd );
+
+		dex::string url = "hamiltoncshell.com";
+		dex::vector < dex::string > title = { "hamilton", "c", "shell" };
+		dex::string titleString = "Hamilton C Shell 2012";
+		dex::vector < dex::string > body = { "some", "junk", "and", "more", "junk" };
+
+		initializingIndexChunk.addDocument( url, title, titleString, body );
+		}
+
+	indexChunkPointers.pushBack( new dex::index::indexChunk( fd, false ) );
+
+	close( fd );
+
+	dex::index::indexChunk *chunkPointer = indexChunkPointers.front( );
+	dex::index::indexChunk::indexStreamReader andISR = dex::index::indexChunk::indexStreamReader( chunkPointer );
+	assert( andISR.next( ) == 2 );
+	assert( andISR.next( ) == 5 );
+	assert( andISR.next( ) == static_cast < size_t >( -1 ) );
 
 
 	// Test Case 1: Query that we expect to find documents for
 	// TODO make a destructor for matchedDocuments objects
 	// TODO make the string that the parser takes in a const reference.
-	dex::string query = "apple";
+	dex::string query = "junk";
 	dex::queryCompiler::parser parser;
 	dex::queryCompiler::matchedDocumentsGenerator mdg = parser.parse( query );
 	dex::queryCompiler::matchedDocuments *testdocs = mdg( indexChunkPointers.front( ) );
-	std::cout << "Asserting that the matchingDocumentsISR for 'apple' is not empty" << std::endl;
+	std::cout << "Asserting that the matchingDocumentsISR for 'junk' is not empty" << std::endl;
 	size_t isrLength = 0;
 	while( testdocs->matchingDocumentISR->next( ) != dex::constraintSolver::ISR::npos )
 		++isrLength;
