4d9126e58632d5d5cb974682b928eca80cf4df11
Chris Combs <combsc@umich.edu>
2020-01-04 14:05:02 -0500

    Update topN test
    combsc

--- src/ranker/topN.test.cpp
+++ src/ranker/topN.test.cpp
@@ -22,7 +22,8 @@ TEST_CASE( "topN" )
 			REQUIRE( topN[ index ]->score == static_cast< double >( 1499 - index ) / 2 );
 
 		for ( size_t index = 0;  index < N;  ++index )
-			delete topN[ index ];
+			if ( topN[ index ] )
+				delete topN[ index ];
 		delete [ ] topN;
 		}
 
@@ -44,7 +45,31 @@ TEST_CASE( "topN" )
 		REQUIRE( topN[ 4 ]->documentIndex == 8 );
 
 		for ( size_t index = 0;  index < N;  ++index )
-			delete topN[ index ];
+			if ( topN[ index ] )
+				delete topN[ index ];
+		delete [ ] topN;
+		}
+
+	SECTION( "n more than the number of scores" )
+		{
+		dex::vector< double > scores = { 0, 5, 7, 7, 2, 1, 5, 6, 6, 4, 9 };
+
+		size_t N = 500;
+		dex::documentInfo **topN = dex::findTopN( scores, N );
+		REQUIRE( topN[ 0 ]->score == 9 );
+		REQUIRE( topN[ 0 ]->documentIndex == 10 );
+		REQUIRE( topN[ 1 ]->score == 7 );
+		REQUIRE( topN[ 1 ]->documentIndex == 2 );
+		REQUIRE( topN[ 2 ]->score == 7 );
+		REQUIRE( topN[ 2 ]->documentIndex == 3 );
+		REQUIRE( topN[ 3 ]->score == 6 );
+		REQUIRE( topN[ 3 ]->documentIndex == 7 );
+		REQUIRE( topN[ 4 ]->score == 6 );
+		REQUIRE( topN[ 4 ]->documentIndex == 8 );
+
+		for ( size_t index = 0;  index < N;  ++index )
+			if ( topN[ index ] )
+				delete topN[ index ];
 		delete [ ] topN;
 		}
 	}
