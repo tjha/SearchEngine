42d0149bd066ef7925fbc8d5a3313a5750176f26
Stephen Jasina <jasina@umich.edu>
2020-01-05 17:52:00 -0500

    Address PR comments
    jasina

--- src/frontend/cli.cpp
+++ src/frontend/cli.cpp
@@ -20,8 +20,8 @@ int main( int argc, char **argv )
 		return 1;
 		}
 	dex::vector< dex::string > indexChunkFilenames = dex::matchingFilenames( argv[ 1 ], "_in.dex" );
-	dex::vector< dex::index::indexChunk * > indexChunkObjects;
-	indexChunkObjects.reserve( indexChunkFilenames.size( ) );
+	dex::vector< dex::index::indexChunk * > indexChunks;
+	indexChunks.reserve( indexChunkFilenames.size( ) );
 
 	if ( indexChunkFilenames.empty( ) )
 		{
@@ -29,13 +29,24 @@ int main( int argc, char **argv )
 		return 1;
 		}
 	std::cout << "Found " << indexChunkFilenames.size( ) << " index chunks" << std::endl;
-	std::cout << "Please enter ':wq' to stop searching" << std::endl;
+	std::cout << "Please enter ':wq' or press Ctrl-D to stop searching" << std::endl;
 
 	for ( size_t index = 0;  index < indexChunkFilenames.size( );  ++index )
 		{
 		int fd = open( indexChunkFilenames[ index ].cStr( ), O_RDWR, 0777 );
-		indexChunkObjects.pushBack( new dex::index::indexChunk( fd, false ) );
-		close( fd );
+		if ( fd == -1 )
+			std::cout << "failed to open chunk " << indexChunkFilenames[ index ] << ". Continuing anyways." << std::endl;
+		else
+			{
+			indexChunks.pushBack( new dex::index::indexChunk( fd, false ) );
+			close( fd );
+			}
+		}
+
+	if ( indexChunks.empty( ) )
+		{
+		std::cout << "No index chunks were able to be opened. Exiting." << std::endl;
+		return 1;
 		}
 
 	std::string input;
@@ -44,37 +55,43 @@ int main( int argc, char **argv )
 
 	while ( true )
 		{
+		std::cout << dex::string( 80, '-' ) << std::endl;
 		std::cout << "Enter query: ";
+
+		// Check whether Ctrl-D was pressed
 		if ( !std::getline( std::cin, input ) )
 			break;
 
 		query = dex::string( input.data( ) );
 		if ( query == ":wq" || query == ":q!" || query == "Michigan Alabama Halftime Score" )
-			{
-			std::cout << "Exiting Search Engine" << std::endl;
-			return 0;
-			}
+			break;
+
 		dex::pair< dex::vector< dex::ranker::searchResult >, int > searchResultsPair
-				= dex::ranker::getTopN( 10, query, &rankerObject, indexChunkObjects );
+				= dex::ranker::getTopN( 10, query, &rankerObject, indexChunks );
 
 		if ( searchResultsPair.second == -1 )
-			std::cout << "\nQuery passed in was malformed" << std::endl;
+			std::cout << "Query passed in was malformed" << std::endl;
 		else
 			{
-			dex::vector< dex::ranker::searchResult > searchResults = searchResultsPair.first;
-			for ( size_t result = 0;  result < searchResults.size( );  ++result )
+			if ( searchResultsPair.first.empty( ) )
+				std::cout << "No results found" << std::endl;
+			else
 				{
-				std::cout << "DOCUMENT " << result + 1 << ":\n";
-				std::cout << searchResults[ result ].title << '\n' << searchResults[ result ].url.completeUrl( )
-						<< '\n' << searchResults[ result ].score << '\n' << std::endl;
-				}
+				dex::vector< dex::ranker::searchResult > searchResults = searchResultsPair.first;
+				for ( size_t result = 0;  result < searchResults.size( );  ++result )
+					{
+					std::cout << "DOCUMENT " << result + 1 << std::endl;
+					std::cout << '\t' << searchResults[ result ].title << std::endl;
+					std::cout << '\t' << searchResults[ result ].url.completeUrl( ) << std::endl;
+					std::cout << '\t' << searchResults[ result ].score << std::endl;
+					}
 			}
 		}
 
-	std::cout << std::endl;
+	while ( !indexChunks.empty( ) )
+		delete indexChunks.back( );
 
-	while ( !indexChunkObjects.empty( ) )
-		delete indexChunkObjects.back( );
+	std::cout << "Exiting Search Engine" << std::endl;
 
 	return 0;
 	}
